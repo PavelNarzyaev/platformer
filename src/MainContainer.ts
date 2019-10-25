925cf2527df9430386dddaf07f5200c057e17b45
Pavel Narzyaev <pavel@narzyaev.ru>
2019-10-25 19:43:23 +0300

    added Player.ts and direction

--- src/MainContainer.ts
+++ src/MainContainer.ts
@@ -1,5 +1,7 @@
 import {View} from "./View";
 import {addEvent} from "./Globals";
+import Loader = PIXI.loaders.Loader;
+import Player from "./Player";
 
 export default class MainContainer extends View {
 	private static readonly UP:string = "ArrowUp";
@@ -12,13 +14,32 @@ export default class MainContainer extends View {
 	private _pressedLeft:boolean = false;
 	private _pressedRight:boolean = false;
 
+	private _player:Player;
+
 	constructor() {
 		super();
 		this.showTestBackground(0xCFCFCF);
 	}
 
 	public init():void {
+		this.startLoading();
+	}
+
+	private startLoading():void {
+		const loader:Loader = new Loader;
+		loader.add(Player.SKIN_NAME);
+		loader.onComplete.add(() => { this.completeLoadingHandler(); });
+		loader.load();
+	}
+
+	private completeLoadingHandler():void {
+		this.initPlayer();
 		this.addKeyListeners();
+	}
+
+	private initPlayer():void {
+		this._player = new Player();
+		this.addChild(this._player);
 	}
 
 	private addKeyListeners():void {
@@ -51,10 +72,12 @@ export default class MainContainer extends View {
 
 			case MainContainer.LEFT:
 				this._pressedLeft = true;
+				this._player.setDirection(Player.LEFT_DIRECTION);
 				break;
 
 			case MainContainer.RIGHT:
 				this._pressedRight = true;
+				this._player.setDirection(Player.RIGHT_DIRECTION);
 				break;
 		}
 	}
---
+++ src/Player.ts
@@ -0,0 +1,38 @@
+import Container = PIXI.Container;
+import Sprite = PIXI.Sprite;
+
+export default class Player extends Container {
+	public static readonly LEFT_DIRECTION:string = "leftDirection";
+	public static readonly RIGHT_DIRECTION:string = "rightDirection";
+
+	public static readonly SKIN_NAME:string = "img/player.png";
+	private _skin:Sprite;
+	private _direction:string = Player.RIGHT_DIRECTION;
+
+	constructor() {
+		super();
+		this.initSkin();
+	}
+
+	private initSkin():void {
+		this._skin = Sprite.from(Player.SKIN_NAME);
+		this.addChild(this._skin);
+	}
+
+	public setDirection(value:string):void {
+		if (this._direction !== value) {
+			this._direction = value;
+			switch (this._direction) {
+				case Player.LEFT_DIRECTION:
+					this._skin.scale.x = -1;
+					this._skin.x = this._skin.width;
+					break;
+
+				case Player.RIGHT_DIRECTION:
+					this._skin.scale.x = 1;
+					this._skin.x = 0;
+					break;
+			}
+		}
+	}
+}
