5cb63baefd81d71d25be22ae9c48cc0549a7fc0d
Pavel Narzyaev <pavel@narzyaev.ru>
2019-10-29 18:47:06 +0300

    added sprite sheet

--- src/Player.ts
+++ src/Player.ts
@@ -1,8 +1,10 @@
 import Container = PIXI.Container;
 import Sprite = PIXI.Sprite;
+import Rectangle = PIXI.Rectangle;
+import Texture = PIXI.Texture;
 
 export default class Player extends Container {
-	public static readonly SKIN_NAME:string = "img/player.png";
+	public static readonly SKIN_NAME:string = "img/alien.png";
 	public static readonly GRAVITY:number = 0.15;
 	public static readonly JUMP_SPEED:number = -8;
 	public static readonly MOVING_SPEED:number = 4;
@@ -13,17 +15,29 @@ export default class Player extends Container {
 	public canJump:boolean = true;
 
 	private _skin:Sprite;
+	private _skinFlipContainer:Container;
 	private _movingDirection:symbol = null;
 	private _skinDirection:symbol = Player.RIGHT;
 
 	constructor() {
 		super();
+		this.initSkinFlipContainer();
 		this.initSkin();
 	}
 
+	private initSkinFlipContainer():void {
+		this._skinFlipContainer = new Container();
+		this.addChild(this._skinFlipContainer);
+	}
+
 	private initSkin():void {
-		this._skin = Sprite.from(Player.SKIN_NAME);
-		this.addChild(this._skin);
+		this._skin = new Sprite(
+			new Texture(
+				Texture.from(Player.SKIN_NAME).baseTexture,
+				new Rectangle(0, 0, 358, 147)
+			)
+		);
+		this._skinFlipContainer.addChild(this._skin);
 	}
 
 	public moveLeft():void {
@@ -45,13 +59,13 @@ export default class Player extends Container {
 			this._skinDirection = this._movingDirection;
 			switch (this._skinDirection) {
 				case Player.LEFT:
-					this._skin.scale.x = -1;
-					this._skin.x = this._skin.width;
+					this._skinFlipContainer.scale.x = -1;
+					this._skinFlipContainer.x = this._skin.width;
 					break;
 
 				case Player.RIGHT:
-					this._skin.scale.x = 1;
-					this._skin.x = 0;
+					this._skinFlipContainer.scale.x = 1;
+					this._skinFlipContainer.x = 0;
 					break;
 			}
 		}
