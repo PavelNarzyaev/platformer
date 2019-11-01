fadda2fe8b42c05e610e39bc1e9c6b4e05832883
Pavel Narzyaev <pavel@narzyaev.ru>
2019-11-01 18:42:31 +0300

    added blocks dragging

--- src/LevelContainer.ts
+++ src/LevelContainer.ts
@@ -8,6 +8,9 @@ import Texture = PIXI.Texture;
 import Rectangle = PIXI.Rectangle;
 import {pixiLoading, xhrJsonLoading} from "./Promises";
 import {addEvent, default as Globals} from "./Globals";
+import {POINTER_DOWN, POINTER_MOVE, POINTER_UP, POINTER_UP_OUTSIDE} from "./PointerEvents";
+import InteractionEvent = PIXI.interaction.InteractionEvent;
+import Point = PIXI.Point;
 
 export default class LevelContainer extends View {
 	private static readonly UP:string = "ArrowUp";
@@ -16,12 +19,19 @@ export default class LevelContainer extends View {
 	private static readonly RIGHT:string = "ArrowRight";
 	private static readonly BACKQUOTE:string = "Backquote";
 	private _pressedButtons:Map<string, boolean> = new Map<string, boolean>();
-	private _blocks:Graphics[] = [];
+	private _blocksHits:Graphics[] = [];
+	private _backSkins:Map<Graphics, Sprite> = new Map<Graphics, Sprite>();
+	private _frontSkins:Map<Graphics, Sprite> = new Map<Graphics, Sprite>();
+	private _blocks:Map<Graphics, IBlock> = new Map<Graphics, IBlock>();
 	private _blocksTypes:Map<string, IType> = new Map<string, IType>();
 	private _backContainer:Container;
 	private _frontContainer:Container;
 	private _frontTexturesCache:Map<string, Texture> = new Map<string, Texture>();
 	private _level:ILevel;
+
+	// DEVELOP_MODE
+	private _draggedBlock:Graphics;
+	private _draggedBlockShiftPoint:Point;
 
 	constructor(
 		private _player:Player,
@@ -83,19 +93,22 @@ export default class LevelContainer extends View {
 
 	private initBlock(block:IBlock):void {
 		const blockType:IType = this._blocksTypes.get(block.type);
-		const newBlock:Graphics = new Graphics();
-		newBlock.x = block.x;
-		newBlock.y = block.y;
-		newBlock.beginFill(0x000000, 0);
-		newBlock.drawRect(0, 0, blockType.hit.width, blockType.hit.height);
-		newBlock.endFill();
-		this.addChild(newBlock);
-		this._blocks.push(newBlock);
+		const newBlockHit:Graphics = new Graphics();
+		newBlockHit.x = block.x;
+		newBlockHit.y = block.y;
+		newBlockHit.beginFill(0x000000, 1);
+		newBlockHit.drawRect(0, 0, blockType.hit.width, blockType.hit.height);
+		newBlockHit.endFill();
+		// newBlockHit.alpha = 0;
+		this.addChild(newBlockHit);
+		this._blocksHits.push(newBlockHit);
+		this._blocks.set(newBlockHit, block);
 
 		const backSkin:Sprite = Sprite.from(blockType.image);
 		backSkin.x = block.x - blockType.hit.x;
 		backSkin.y = block.y - blockType.hit.y;
 		this._backContainer.addChild(backSkin);
+		this._backSkins.set(newBlockHit, backSkin);
 
 		const front:IFront = blockType.front;
 		let frontTexture:Texture;
@@ -109,9 +122,10 @@ export default class LevelContainer extends View {
 			frontTexture = this._frontTexturesCache.get(blockType.id);
 		}
 		const frontSkin:Sprite = new Sprite(frontTexture);
-		frontSkin.x = backSkin.x;
-		frontSkin.y = backSkin.y + 15;
+		frontSkin.x = backSkin.x + blockType.front.x;
+		frontSkin.y = backSkin.y + blockType.front.y;
 		this._frontContainer.addChild(frontSkin);
+		this._frontSkins.set(newBlockHit, frontSkin);
 	}
 
 	private addKeyListeners():void {
@@ -152,12 +166,12 @@ export default class LevelContainer extends View {
 		let limitX:number;
 		switch (this._player.getMovingDirection()) {
 			case Player.LEFT:
-				this._blocks.forEach((block:Graphics) => {
-					limitX = block.x + block.width - this._player.collisionLeft();
+				this._blocksHits.forEach((blockHit:Graphics) => {
+					limitX = blockHit.x + blockHit.width - this._player.collisionLeft();
 					if (
 						this._player.x >= limitX &&
 						this._player.x - Player.MOVING_SPEED < limitX &&
-						HitTest.vertical(this._player, block)
+						HitTest.vertical(this._player, blockHit)
 					) {
 						this._player.x = limitX;
 						canMove = false;
@@ -169,12 +183,12 @@ export default class LevelContainer extends View {
 				break;
 
 			case Player.RIGHT:
-				this._blocks.forEach((block:Graphics) => {
-					limitX = block.x - this._player.collisionRight();
+				this._blocksHits.forEach((blockHit:Graphics) => {
+					limitX = blockHit.x - this._player.collisionRight();
 					if (
 						this._player.x <= limitX &&
 						this._player.x + Player.MOVING_SPEED > limitX &&
-						HitTest.vertical(this._player, block)
+						HitTest.vertical(this._player, blockHit)
 					) {
 						this._player.x = limitX;
 						canMove = false;
@@ -191,12 +205,12 @@ export default class LevelContainer extends View {
 		let limitY:number;
 		this._player.speedY += Player.GRAVITY;
 		if (this._player.speedY > 0) {
-			this._blocks.forEach((block:Graphics) => {
-				limitY = block.y - this._player.collisionBottom();
+			this._blocksHits.forEach((blockHit:Graphics) => {
+				limitY = blockHit.y - this._player.collisionBottom();
 				if (
 					this._player.y <= limitY &&
 					this._player.y + this._player.speedY > limitY &&
-					HitTest.horizontal(this._player, block)
+					HitTest.horizontal(this._player, blockHit)
 				) {
 					this._player.y = limitY;
 					this._player.canJump = true;
@@ -208,12 +222,12 @@ export default class LevelContainer extends View {
 				this._player.y += this._player.speedY;
 			}
 		} else if (this._player.speedY < 0) {
-			this._blocks.forEach((block:Graphics) => {
-				limitY = block.y + block.height - this._player.collisionTop();
+			this._blocksHits.forEach((blockHit:Graphics) => {
+				limitY = blockHit.y + blockHit.height - this._player.collisionTop();
 				if (
 					this._player.y >= limitY &&
 					this._player.y + this._player.speedY < limitY &&
-					HitTest.horizontal(this._player, block)
+					HitTest.horizontal(this._player, blockHit)
 				) {
 					this._player.y = limitY;
 					this._player.speedY = 0;
@@ -250,7 +264,64 @@ export default class LevelContainer extends View {
 
 		if (e.code === LevelContainer.BACKQUOTE && !process.env.RELEASE) {
 			Globals.developerMode.set(!Globals.developerMode.get());
+
+			if (Globals.developerMode.get()) {
+				this.enableDeveloperMode();
+			} else {
+				this.disableDeveloperMode();
+			}
 			console.log("Developer mode is " + (Globals.developerMode.get() ? "ON" : "OFF"));
+		}
+	}
+
+	private enableDeveloperMode():void {
+		this._blocksHits.forEach((blockHit:Graphics) => {
+			blockHit.interactive = true;
+			blockHit.addListener(POINTER_DOWN, this.blockPointerDownHandler, this);
+		});
+	}
+
+	private blockPointerDownHandler(event:InteractionEvent):void {
+		this._draggedBlock = event.currentTarget as Graphics;
+		this._draggedBlockShiftPoint = this._draggedBlock.toLocal(new Point(event.data.global.x, event.data.global.y));
+		this._draggedBlock.addListener(POINTER_MOVE, this.blockPointerMoveHandler, this);
+		this._draggedBlock.addListener(POINTER_UP, this.blockPointerUpHandler, this);
+		this._draggedBlock.addListener(POINTER_UP_OUTSIDE, this.blockPointerUpHandler, this);
+	}
+
+	private blockPointerMoveHandler(event:InteractionEvent):void {
+		const containerPoint:Point = this.toLocal(new Point(event.data.global.x, event.data.global.y));
+		this._draggedBlock.x = Math.round(containerPoint.x - this._draggedBlockShiftPoint.x);
+		this._draggedBlock.y = Math.round(containerPoint.y - this._draggedBlockShiftPoint.y);
+
+		const blockType:IType = this._blocksTypes.get(this._blocks.get(this._draggedBlock).type);
+
+		const backSkin:Sprite = this._backSkins.get(this._draggedBlock);
+		backSkin.x = this._draggedBlock.x - blockType.hit.x;
+		backSkin.y = this._draggedBlock.y - blockType.hit.y;
+
+		const frontSkin:Sprite = this._frontSkins.get(this._draggedBlock);
+		frontSkin.x = backSkin.x + blockType.front.x;
+		frontSkin.y = backSkin.y + blockType.front.y;
+	}
+
+	private blockPointerUpHandler():void {
+		this._draggedBlock.removeAllListeners(POINTER_MOVE);
+		this._draggedBlock.removeAllListeners(POINTER_UP);
+		this._draggedBlock.removeAllListeners(POINTER_UP_OUTSIDE);
+		this._draggedBlock = null;
+	}
+
+	private disableDeveloperMode():void {
+		this._blocksHits.forEach((blockHit:Graphics) => {
+			blockHit.interactive = false;
+			blockHit.removeAllListeners(POINTER_DOWN);
+			blockHit.removeAllListeners(POINTER_UP);
+			blockHit.removeAllListeners(POINTER_UP_OUTSIDE);
+		});
+
+		if (this._draggedBlock) {
+			this._draggedBlock.removeAllListeners(POINTER_MOVE);
 		}
 	}
 
