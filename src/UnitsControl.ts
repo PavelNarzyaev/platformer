46c1d0f8dc1fc4fc10d8f05d05a59b43f3f88a46
Pavel Narzyaev <pavel@narzyaev.ru>
2019-11-05 02:51:51 +0300

    fixed collision detection bug

--- src/UnitsControl.ts
+++ src/UnitsControl.ts
@@ -15,12 +15,12 @@ export default class UnitsControl {
 	}
 
 	private horizontalMoving():void {
-		let canMove:boolean = true;
 		let limitX:number;
 		let targetX:number;
 		switch (this._player.getMovingDirection()) {
 			case Player.LEFT:
 				targetX = Math.round(this._player.x - Player.MOVING_SPEED);
+				let maxLimitX:number = null;
 				this._blocks.forEach((block:Block) => {
 					limitX = block.collisionRight() - this._player.localCollisionLeft();
 					if (
@@ -28,17 +28,15 @@ export default class UnitsControl {
 						targetX < limitX &&
 						HitTest.vertical(this._player, block)
 					) {
-						this._player.x = limitX;
-						canMove = false;
+						maxLimitX = maxLimitX !== null ? Math.max(maxLimitX, limitX) : limitX;
 					}
 				});
-				if (canMove) {
-					this._player.x = targetX;
-				}
+				this._player.x = maxLimitX !== null ? maxLimitX : targetX;
 				break;
 
 			case Player.RIGHT:
 				targetX = Math.round(this._player.x + Player.MOVING_SPEED);
+				let minLimitX:number = null;
 				this._blocks.forEach((block:Block) => {
 					limitX = block.collisionLeft() - this._player.localCollisionRight();
 					if (
@@ -46,13 +44,10 @@ export default class UnitsControl {
 						targetX > limitX &&
 						HitTest.vertical(this._player, block)
 					) {
-						this._player.x = limitX;
-						canMove = false;
+						minLimitX = minLimitX !== null ? Math.min(minLimitX, limitX) : limitX;
 					}
 				});
-				if (canMove) {
-					this._player.x = targetX;
-				}
+				this._player.x = minLimitX !== null ? minLimitX : targetX;
 				break;
 		}
 	}
@@ -62,6 +57,7 @@ export default class UnitsControl {
 		let limitY:number;
 		let targetY:number = this._player.y + this._player.speedY;
 		if (this._player.speedY > 0) {
+			let minLimitY:number = null;
 			this._blocks.forEach((block:Block) => {
 				limitY = block.collisionTop() - this._player.localCollisionBottom();
 				if (
@@ -69,16 +65,19 @@ export default class UnitsControl {
 					targetY > limitY &&
 					HitTest.horizontal(this._player, block)
 				) {
-					this._player.y = limitY;
-					this._player.canJump = true;
-					this._player.speedY = 0;
+					minLimitY = minLimitY !== null ? Math.min(minLimitY, limitY) : limitY;
 				}
 			});
-			if (this._player.speedY !== 0) {
+			if (minLimitY !== null) {
+				this._player.y = minLimitY;
+				this._player.canJump = true;
+				this._player.speedY = 0;
+			} else {
 				this._player.canJump = false;
 				this._player.y = targetY;
 			}
 		} else if (this._player.speedY < 0) {
+			let maxLimitY:number = null;
 			this._blocks.forEach((block:Block) => {
 				limitY = block.collisionBottom() - this._player.localCollisionTop();
 				if (
@@ -86,11 +85,13 @@ export default class UnitsControl {
 					targetY < limitY &&
 					HitTest.horizontal(this._player, block)
 				) {
-					this._player.y = limitY;
-					this._player.speedY = 0;
+					maxLimitY = maxLimitY !== null ? Math.max(maxLimitY, limitY) : limitY;
 				}
 			});
-			if (this._player.speedY !== 0) {
+			if (maxLimitY !== null) {
+				this._player.y = maxLimitY;
+				this._player.speedY = 0;
+			} else {
 				this._player.y = targetY;
 			}
 		}
