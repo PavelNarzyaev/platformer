de7e71baf21ae3c1b46de94aa5e2f4edf2bb721c
Pavel Narzyaev <pavel@narzyaev.ru>
2019-11-13 19:19:14 +0300

    added platform block

--- src/MainContainer.ts
+++ src/MainContainer.ts
@@ -40,7 +40,7 @@ export default class MainContainer extends View {
 	private initLevelContainer():void {
 		this._levelContainer = new Level(
 			this._player,
-			"levels/level_1.json"
+			"levels/level_2.json"
 		);
 		this._levelContainer.setSize(2000, 2000);
 		this.addChild(this._levelContainer);
